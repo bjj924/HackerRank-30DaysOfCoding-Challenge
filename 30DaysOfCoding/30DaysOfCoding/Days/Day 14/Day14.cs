f1e55f7b626b6d640553b779b344435db29a4677
BraianJ <joho.braian@gmail.com>
2020-09-26 21:10:11 -0300

    Day 14

---
+++ 30DaysOfCoding/30DaysOfCoding/Days/Day 14/Day14.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _30DaysOfCoding.Days.Day_14
+{
+    public class Day14
+    {
+        public static void AbsoluteNumber()
+        {
+            Convert.ToInt32(Console.ReadLine());
+
+            int[] a = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+            Difference d = new Difference(a);
+
+            d.computeDifference();
+
+            Console.Write(d.maximumDifference);
+        }
+    }
+}
---
+++ 30DaysOfCoding/30DaysOfCoding/Days/Day 14/Difference.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _30DaysOfCoding.Days.Day_14
+{
+    public class Difference
+    {
+        private int[] elements;
+        public int maximumDifference;
+
+        public Difference() { }
+
+        public Difference(int[] elements)
+        {
+            this.elements = elements;
+        }
+
+        public void computeDifference() 
+        {
+            Array.Sort(elements);
+
+            maximumDifference = Math.Abs(elements[elements.Length - 1]) - Math.Abs(elements[0]);
+
+        }
+    }
+}
