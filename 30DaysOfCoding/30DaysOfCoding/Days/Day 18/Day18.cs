04fabbb1fa674c9eaa1634a3411bb9e01fee8896
BraianJ <joho.braian@gmail.com>
2020-09-30 15:50:48 -0300

    Day 18 finished

--- 30DaysOfCoding/30DaysOfCoding/Days/Day 18/Day18.cs
+++ 30DaysOfCoding/30DaysOfCoding/Days/Day 18/Day18.cs
@@ -1,10 +1,44 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Text;
 
 namespace _30DaysOfCoding.Days.Day_18
 {
-    class Day18
+    public class Day18
     {
+        public static void Palindrome()
+        {
+            string s = Console.ReadLine();
+
+            Solution obj = new Solution();
+
+            foreach (char c in s)
+            {
+                obj.pushCharacter(c);
+                obj.enqueueCharacter(c);
+            }
+
+            bool isPalindrome = true;
+
+            for (int i = 0; i < s.Length / 2; i++)
+            {
+                if (obj.popCharacter() != obj.dequeueCharacter())
+                {
+                    isPalindrome = false;
+
+                    break;
+                }
+            }
+
+            if (isPalindrome)
+            {
+                Console.Write("The word, {0}, is a palindrome.", s);
+            }
+            else
+            {
+                Console.Write("The word, {0}, is not a palindrome.", s);
+            }
+        }
     }
 }
---
+++ 30DaysOfCoding/30DaysOfCoding/Days/Day 18/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _30DaysOfCoding.Days.Day_18
+{
+    public class Solution
+    {
+        public Stack<char> charS = new Stack<char>();
+
+        public Queue<char> charQ = new Queue<char>();
+        public void pushCharacter(char c)
+        {
+            charS.Push(c);
+        }
+
+        public char popCharacter()
+        {
+            return charS.Pop();
+        }
+        public void enqueueCharacter(char c)
+        {
+            charQ.Enqueue(c);
+        }
+
+        public char dequeueCharacter()
+        {
+            return charQ.Dequeue();
+        }
+    }
+}
