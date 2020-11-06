4c25aea32400e6125071c31ded7d24c586988da8
BraianJ <joho.braian@gmail.com>
2020-11-06 18:51:54 -0300

    Day 29 - Last day of the challenge

--- 30DaysOfCoding/30DaysOfCoding/DaySwitch.cs
+++ 30DaysOfCoding/30DaysOfCoding/DaySwitch.cs
@@ -12,6 +12,7 @@ using _30DaysOfCoding.Days.Day_24;
 using _30DaysOfCoding.Days.Day_25;
 using _30DaysOfCoding.Days.Day_26;
 using _30DaysOfCoding.Days.Day_28;
+using _30DaysOfCoding.Days.Day_29;
 using _30DaysOfCoding.Days.Day0;
 using _30DaysOfCoding.Days.Day1;
 using _30DaysOfCoding.Days.Day10;
@@ -119,6 +120,9 @@ namespace _30DaysOfCoding
                     break;
                 case 28:
                     Day_28.RegexTester();
+                    break;
+                case 29:
+                    Day29.BitwiseAND();
                     break;
                 default:
                     Console.WriteLine("Coming soon...");
---
+++ 30DaysOfCoding/30DaysOfCoding/Days/Day 29/Day29.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _30DaysOfCoding.Days.Day_29
+{
+    public class Day29
+    {
+        public static void BitwiseAND()
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            for (int a0 = 0; a0 < t; a0++)
+            {
+                string[] tokens_n = Console.ReadLine().Split(' ');
+                int n = Convert.ToInt32(tokens_n[0]);
+                int k = Convert.ToInt32(tokens_n[1]);
+
+                if (((k - 1) | k) > n && k % 2 == 0)
+                    Console.WriteLine(k - 2);
+                else
+                    Console.WriteLine(k - 1);
+            }
+        }
+    }
+}
